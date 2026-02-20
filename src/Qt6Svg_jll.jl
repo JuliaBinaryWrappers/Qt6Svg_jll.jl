e4beea41e26701ecd2604f793797eb849740841f
jlbuild <juliabuildbot@gmail.com>
2026-02-20 18:30:51 +0000

    Qt6Svg_jll build 6.10.2+0

--- src/Qt6Svg_jll.jl
+++ src/Qt6Svg_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt6Svg")
-JLLWrappers.@generate_main_file("Qt6Svg", UUID("6de9746b-f93d-5813-b365-ba18ad4a9cf3"))
+JLLWrappers.@generate_main_file("Qt6Svg", Base.UUID("6de9746b-f93d-5813-b365-ba18ad4a9cf3"))
 end  # module Qt6Svg_jll
